0982f35965a879d49973edbddc6b3e3423108218
germandefrancesco <germandefrancesco@hotmail.com>
2022-11-23 15:55:13 -0300

    Add pay button with mock data

---
+++ BlazorMercadoPagoDemo/Data/MercadoPagoService.cs
@@ -0,0 +1,45 @@
+﻿using MercadoPago.Client.Preference;
+
+namespace BlazorMercadoPagoDemo.Data;
+
+public interface IMercadoPagoService
+{
+    Task<string> GenerateMercadoPagoLink();
+}
+
+public class MercadoPagoService : IMercadoPagoService
+{
+    public async Task<string> GenerateMercadoPagoLink()
+    {
+        var preferenceRequest = new PreferenceRequest();
+        preferenceRequest.Items = new List<PreferenceItemRequest>
+        {
+            new PreferenceItemRequest
+            {
+                Id = "1234",
+                Title = "Invoice",
+                Quantity = 1,
+                CurrencyId = "ARS",
+                UnitPrice = 500
+            },
+        };
+        preferenceRequest.BackUrls = new PreferenceBackUrlsRequest()
+        {
+            Success = $"localhost:7218/?status=success",
+            Failure = $"localhost:7218/?status=failure",
+            Pending = $"localhost:7218/?status=pending"
+        };
+        preferenceRequest.PaymentMethods = new PreferencePaymentMethodsRequest()
+        {
+            Installments = 1
+        };
+        preferenceRequest.AutoReturn = "approved";
+        preferenceRequest.NotificationUrl = $"localhost:7218/api/notifications";
+        preferenceRequest.Expires = true;
+        preferenceRequest.ExpirationDateTo = DateTime.Now.AddDays(1);
+
+        var preferenceClient = new PreferenceClient();
+        var preference = await preferenceClient.CreateAsync(preferenceRequest);
+        return preference.InitPoint;
+    }
+}
--- BlazorMercadoPagoDemo/Program.cs
+++ BlazorMercadoPagoDemo/Program.cs
@@ -1,26 +1,26 @@
+using BlazorMercadoPagoDemo.Data;
+using MercadoPago.Config;
+
+MercadoPagoConfig.AccessToken = Environment.GetEnvironmentVariable("MercadoPagoTest");
+//MercadoPagoConfig.AccessToken = Environment.GetEnvironmentVariable("MercadoPagoProd");
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<IMercadoPagoService, MercadoPagoService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
